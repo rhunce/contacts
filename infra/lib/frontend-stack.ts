ab24d2103e2ecdb4ffccd6a9e52b72362e6ac7df
Raymond Hunce <hunce18c@gmail.com>
2025-08-28 12:49:35 -0400

    Create new CF Dist

--- infra/lib/frontend-stack.ts
+++ infra/lib/frontend-stack.ts
@@ -33,7 +33,7 @@ export class FrontendStack extends cdk.Stack {
     this.bucket.grantRead(originAccessIdentity);
 
     // CloudFront Distribution
-    this.distribution = new cloudfront.Distribution(this, `${props.appName}FrontendDistribution`, {
+    this.distribution = new cloudfront.Distribution(this, `${props.appName}FrontendDistributionV2`, {
       defaultBehavior: {
         origin: new origins.S3Origin(this.bucket, {
           originAccessIdentity,
