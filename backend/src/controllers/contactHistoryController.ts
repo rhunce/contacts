4422d796a6296c51b5f36a3e4c0fde672cfef9e9
Raymond Hunce <hunce18c@gmail.com>
2025-09-01 00:22:00 -0400

    Bug fix

--- backend/src/controllers/contactHistoryController.ts
+++ backend/src/controllers/contactHistoryController.ts
@@ -14,7 +14,7 @@ export class ContactHistoryController {
       const contactId = req.params.id;
       const { page = 1, limit = 10 } = req.query;
       const result = await this.contactHistoryService.getContactHistory(contactId, req.userId!, Number(page), Number(limit));
-      res.success(result);
+      res.success(result.items);
     } catch (error: any) {
       res.error(error.message);
     }
